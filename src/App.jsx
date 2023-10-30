97843f4455411da8863efc646efcc647fb02119b
fdm1try <d.a.fedorov90@gmail.com>
2023-10-30 15:34:31 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import './App.css';
-import { ShopItemFunc } from './components/ShopItemFunc';
-
-const item = {
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  price: 399,
-  currency: '£'
-}
+import { Portfolio } from './components/Portfolio';
+import { portfolioItems } from './data';
 
 function App() {
+  const items = portfolioItems.map((item, index) => ({id: index,...item}));
   return (
-    <div className="container">
-      <div className="background-element">
-      </div>
-      <div className="highlight-window">
-        <div className='highlight-overlay'></div>
-      </div>
-      <div className="window">
-        <ShopItemFunc item={item} />
-      </div>
-    </div>
+    <>
+      <Portfolio items={items} />
+    </>
   )
 }
 
